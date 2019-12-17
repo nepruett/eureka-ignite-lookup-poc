82ecd72a8b03eaf86965d5084dca2e94091d8f14
nathanep <blood2425>
2019-12-16 19:26:28 -0500

    initial commit

---
+++ ignite-client/src/main/java/nep/eipoc/IgniteClientApplication.java
@@ -0,0 +1,13 @@
+package nep.eipoc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@EnableDiscoveryClient
+@SpringBootApplication
+public class IgniteClientApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(IgniteClientApplication.class, args);
+    }
+}
